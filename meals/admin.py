e6834265fd55cc212cbfa5b26f14fb55d8dcaecd
jmr <jmr@makina-corpus.com>
2020-12-07 00:31:30 +0100

    Admin and app label display

--- meals/admin.py
+++ meals/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from meals.models import Ingredient, IngredientQuantity, Meal
+
+
+class IngredientQuantityInline(admin.TabularInline):
+    model = IngredientQuantity
+    extra = 3
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    fields = ("name",)
+
+
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    fields = ("name", "description", "season", "url", "difficulty")
+    list_display = ("name", "description", "season", "url", "difficulty")
+    list_filter = ("season", "difficulty")
+    inlines = (IngredientQuantityInline,)
--- meals/apps.py
+++ meals/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class MealsConfig(AppConfig):
     name = 'meals'
+    verbose_name = "Repas"
