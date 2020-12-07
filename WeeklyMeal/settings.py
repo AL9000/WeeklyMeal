756d205d52349153abb8b779beb3f94417a80c92
jmr <jmr@makina-corpus.com>
2020-12-08 00:05:53 +0100

    Add django-filter

--- WeeklyMeal/settings.py
+++ WeeklyMeal/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_filters',
     # TODO Split in dev settings
     'debug_toolbar',
     'django_extensions',
