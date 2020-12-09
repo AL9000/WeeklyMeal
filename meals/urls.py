f595318c8aafd2c090002559dc13bc971ed77a7f
jmr <jmr@makina-corpus.com>
2020-12-10 00:34:37 +0100

    CartView with all ingredient list

--- meals/urls.py
+++ meals/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from django_filters.views import FilterView
 
 from meals.filters import MealFilter
-from meals.views import MealDetailView
+from meals.views import MealDetailView, CartView
 
 
-app_name = 'meals'
+app_name = "meals"
 urlpatterns = [
     path("", FilterView.as_view(filterset_class=MealFilter), name="meal-list"),
     path("<int:pk>/", MealDetailView.as_view(), name="meal-detail"),
+    path("cart/", CartView.as_view(), name="cart"),
 ]
--- meals/views.py
+++ meals/views.py
@@ -1,7 +1,25 @@
-from django.views.generic import DetailView
+from django.db.models import Prefetch
+from django.views.generic import DetailView, ListView
 
-from meals.models import Meal
+from meals.models import IngredientQuantity, Meal
 
 
 class MealDetailView(DetailView):
     model = Meal
+
+
+class CartView(ListView):
+    model = Meal
+    template_name = "meals/cart.html"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        meals = self.request.GET.getlist("meal")
+        queryset = queryset.filter(pk__in=meals).prefetch_related(
+            Prefetch(
+                "ingredientquantity_set",
+                queryset=IngredientQuantity.objects.distinct(),
+                to_attr="cart_ingredients",
+            ),
+        )
+        return queryset
