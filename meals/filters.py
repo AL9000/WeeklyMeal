432c636bb444b9b6e76d83cba7d0893b3ec8f6a6
jmr <jmr@makina-corpus.com>
2020-12-12 02:13:57 +0100

    Filter meals over multiple fields

--- meals/filters.py
+++ meals/filters.py
@@ -1,16 +1,17 @@
 import django_filters
 from django import forms
+from django.db.models import Q
 from django.forms import CheckboxSelectMultiple
 
 from meals.models import Meal, Season
 
 
 class MealFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(
-        lookup_expr="icontains",
+    search = django_filters.CharFilter(
+        method="meal_filter",
         widget=forms.TextInput(attrs={
             "class": "form-control",
-            "placeholder": "Nom de recette",
+            "placeholder": "Nom de recette, commentaire, ingrédient",
         })
     )
     seasons = django_filters.ModelMultipleChoiceFilter(
@@ -24,6 +25,13 @@ class MealFilter(django_filters.FilterSet):
         widget=forms.Select(attrs={'class': 'form-control'}),
     )
 
+    def meal_filter(self, queryset, name, value):
+        return Meal.objects.filter(
+            Q(name__icontains=value) |
+            Q(comment__icontains=value) |
+            Q(ingredients__name__icontains=value)
+        ).distinct()
+
     class Meta:
         model = Meal
-        fields = ["name", "seasons", "difficulty"]
+        fields = ["search", "seasons", "difficulty"]
